fe0d8b2b69b3d636491874063f9a974fbbb48812
KeKs0r <marc.hoeffl@gmail.com>
2021-08-11 13:30:32 +0200

    Create a lot of things

---
+++ libs/react-lib/src/lib/react-lib.tsx
@@ -0,0 +1,14 @@
+import './react-lib.module.css';
+
+/* eslint-disable-next-line */
+export interface ReactLibProps {}
+
+export function ReactLib(props: ReactLibProps) {
+  return (
+    <div>
+      <h1>Welcome to ReactLib!</h1>
+    </div>
+  );
+}
+
+export default ReactLib;
